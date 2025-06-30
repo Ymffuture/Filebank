64c7e9ab78531809acb10ba613f1a5771384c13c
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2025-06-30 07:52:51 +0200

    25

--- filebank-frontend/src/api/fileApi.js
+++ filebank-frontend/src/api/fileApi.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 // Create a shared Axios instance
 const api = axios.create({
-  baseURL: 'https://filebankserver.onrender.com/api',
-  
-  // works for /auth, /files, /notifications etc.
+  baseURL: 'https://filebankserver.onrender.com/api', // works for /auth, /files, /notifications etc.
 });
 
 // Request interceptor: Attach token
