c82314e766afb67531f439cc77c82594024b90bc
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-01 06:10:51 +0200

    Update main.jsx

--- filebank-frontend/src/main.jsx
+++ filebank-frontend/src/main.jsx
@@ -6,13 +6,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import {SnackbarProvider} from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SnackbarProvider>
-    <GoogleOAuthProvider clientId="815421801782-jkj6h92h24uo1nhe03hvqfhctebhnaht.apps.googleusercontent.com">
-
+  <React.StrictMode>  
+  <GoogleOAuthProvider clientId="815421801782-jkj6h92h24uo1nhe03hvqfhctebhnaht.apps.googleusercontent.com">
+<SnackbarProvider>
   <App />
-
+</SnackbarProvider>
     </GoogleOAuthProvider>
-   </SnackbarProvider>
   </React.StrictMode>
 );
