aedc22395da9354576845fe9deb08cabe20b0e57
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-03 04:08:21 +0200

    Update FileList.jsx

--- filebank-frontend/src/components/FileList.jsx
+++ filebank-frontend/src/components/FileList.jsx
@@ -5,7 +5,7 @@ import api from '../api/fileApi';
 import { ArrowBigLeftDashIcon } from 'lucide-react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
-
+import FileUpload from "./FileUpload" 
 export default function FileList() {
   const [files, setFiles] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -239,7 +239,7 @@ export default function FileList() {
         ) : (
           <Card hoverable className="text-center text-gray-400" bodyStyle={{ minHeight: 200 }}>
             <p className="text-lg text-gray-400">No files uploaded yet. Start by uploading your first file!</p>
-
+<FileUpload/>
           </Card>
         )}
       </div>
