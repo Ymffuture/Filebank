1d98435195ffa3c58fcc662431fab94897338cb9
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-08 17:06:18 +0200

    Update Ai.jsx

--- filebank-frontend/src/components/Ai.jsx
+++ filebank-frontend/src/components/Ai.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, Input, Typography, Modal, Space } from 'antd';
 import { MessageOutlined } from '@ant-design/icons';
 import api from '../api/fileApi';
-
+import { ArrowRight, ArrowLeft,  ArrowUp} from 'lucide-react';
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
 import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -14,7 +14,7 @@ const { Text } = Typography;
 const ChatBotModal = () => {
   const [visible, setVisible] = useState(false);
   const [messages, setMessages] = useState([
-    { from: 'bot', text: 'Hi! Ask me anything.' },
+    { from: 'bot', text: 'Hi! How filebank AI help? .' },
   ]);
   const [input, setInput] = useState('');
   const [loading, setLoading] = useState(false);
@@ -76,7 +76,7 @@ const ChatBotModal = () => {
               borderRadius: 10,
               background: msg.from === 'user' ? '#1E90FF' : '#f5f5f5',
               color: msg.from === 'user' ? '#fff' : '#000',
-              maxWidth: '85%',
+              maxWidth: '90%',
               wordBreak: 'break-word',
               whiteSpace: 'pre-wrap',
             }}
@@ -107,12 +107,12 @@ const ChatBotModal = () => {
       />
 
       <Modal
-        title="🤖 Chat Assistant"
+        title="Filebank Chat Assistant"
         open={visible}
         onCancel={() => setVisible(false)}
         footer={null}
-        width={450}
-        bodyStyle={{ maxHeight: '60vh', overflowY: 'auto', paddingBottom: 0 }}
+        width={500}
+        bodyStyle={{ maxHeight: '80vh', overflowY: 'auto', paddingBottom: 0 }}
       >
         <div style={{ marginBottom: 12 }}>
           {messages.map((msg, idx) => (
@@ -130,13 +130,13 @@ const ChatBotModal = () => {
 
         <Space.Compact style={{ width: '100%' }}>
           <Input
-            placeholder="Type a message..."
+            placeholder="Ask filebank AI..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
           />
           <Button type="primary" loading={loading} onClick={sendMessage}>
-            Send
+            <ArrowUp/>
           </Button>
         </Space.Compact>
       </Modal>
