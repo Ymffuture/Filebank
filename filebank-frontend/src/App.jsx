0f66a88cce25024d7c59a04e0c9081c92d448758
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-15 07:27:06 +0200

    Update App.jsx

--- filebank-frontend/src/App.jsx
+++ filebank-frontend/src/App.jsx
@@ -29,36 +29,37 @@ const Loader = () => (
     minHeight: '100vh',
     background: '#fff'
   }}>
-    <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <linearGradient id="ratGradient" x1="0%" y1="0%" x2="100%" y2="100%">
       <stop offset="0%" stop-color="#0B3D91" />
+      <stop offset="50%" stop-color="transparent" />
       <stop offset="100%" stop-color="#1E90FF" />
     </linearGradient>
   </defs>
   <circle
     cx="50"
     cy="50"
-    r="45"
+    r="35"
     stroke="url(#ratGradient)"
     stroke-width="6"
     fill="none"
     stroke-linecap="round"
-    stroke-dasharray="282"
-    stroke-dashoffset="210"
+    stroke-dasharray="280"
+    stroke-dashoffset="650"
   >
     <animateTransform
       attributeName="transform"
       type="rotate"
       values="0 50 50; 360 50 50; 0 50 50; -360 50 50; 0 50 50"
       keyTimes="0; 0.25; 0.5; 0.75; 1"
-      dur="6s"
+      dur="10s"
       repeatCount="indefinite"
     />
     <animate
       attributeName="stroke-dashoffset"
       values="210;0;210"
-      dur="1.4s"
+      dur="1.8s"
       repeatCount="indefinite"
     />
   </circle>
