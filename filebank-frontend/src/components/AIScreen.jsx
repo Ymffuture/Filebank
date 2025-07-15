4eacf78aa551c5bdfb53ceec81021b48f8490ad8
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-15 19:12:49 +0200

    Update AIScreen.jsx

--- filebank-frontend/src/components/AIScreen.jsx
+++ filebank-frontend/src/components/AIScreen.jsx
@@ -246,7 +246,7 @@ const sendMessage = async (overrideInput) => {
   return (
     <div className={`${darkMode ? 'bg-[#333]' : ''} h-screen flex flex-col bg-gradient-to-br from-white to-white dark:from-gray-800 dark:to-gray-900`}>
       <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-800">
-        <h1 className="text-[14px] font-bold text-[gray] dark:text-white">FBC AI 3.1.0v</h1>
+        <h1 className="text-[14px] font-bold text-[gray] dark:text-white">FamaAI 3.3.1v</h1>
         <Link to='/dashboard'>Dashboard</Link>
         <Button onClick={clearChat} size="small">Clear</Button>
         <Space>
