a57b1bea65031b70885fb08299e042e00b88d4fa
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-16 18:42:23 +0200

    Update FileUpload.jsx

--- filebank-frontend/src/components/FileUpload.jsx
+++ filebank-frontend/src/components/FileUpload.jsx
@@ -5,7 +5,7 @@ import { UploadOutlined, InfoCircleOutlined, LinkOutlined } from '@ant-design/ic
 import { useSnackbar } from 'notistack';
 import { Helmet } from 'react-helmet';
 import Lottie from 'lottie-react';
-import uploadAnimation from '/uploading.json'; // Place upload.json in src/assets/
+import uploadAnimation from '../assets/uploading.json'; // Place upload.json in src/assets/
 
 import api from '../api/fileApi';
 
