1b50def4a280826d0395b24a7f491f33aafcd45d
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-17 06:29:59 +0200

    Update Home.jsx

--- filebank-frontend/src/pages/Home.jsx
+++ filebank-frontend/src/pages/Home.jsx
@@ -10,18 +10,14 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#fff] to-[#fff]">
       <Navbar />
-      <main className="container mx-auto py-5 space-y-6 px-2">
+      <main className="container mx-auto p-2 space-y-2">
         <section>
-          <h1 className="text-3xl font-bold text-gray-700 mb-0">Start uploading Your Files</h1>
           <FileUpload onUpload={() => setRefresh((r) => r + 1)} />
         </section>
 <section>
   <Ai/>
 </section>
-  
-  
         <section>
-          <h1 className="text-3xl font-bold text-[#222]">Documentary</h1>
           <FileBankDocumentary />
         </section>
       </main>
