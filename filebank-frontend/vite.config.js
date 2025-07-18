ed7e071f37027f8ce5a0f08937f63900b8c80ea8
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-18 06:31:21 +0200

    Update vite.config.js

--- filebank-frontend/vite.config.js
+++ filebank-frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import lexical from 'vite-plugin-lexical';
 
 export default defineConfig({
-  plugins: [react(), lexical()],
+  plugins: [react()]
 });
