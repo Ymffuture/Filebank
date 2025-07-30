c91f72902124c3d984ac04ad742f0fd3531adde7
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-30 03:35:12 +0200

    Update CVTips.jsx

--- filebank-frontend/src/pages/CVTips.jsx
+++ filebank-frontend/src/pages/CVTips.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import CVHero from '../components/CVHero';
 import CVSteps from '../components/CVSteps';
 import PricingSlider from '../components/PricingSlider';
+import Navbar from '../components/Navbar';
 
 const CVTips = () => {
   return (
     <div className="min-h-screen bg-white text-gray-900">
+      <Navbar/>
       <CVHero />
       <CVSteps />
       <PricingSlider />
