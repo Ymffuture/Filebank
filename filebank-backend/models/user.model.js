f7046a26607145f5f8354ed54061457a2fe4970e
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-31 09:11:07 +0200

    Delete filebank-backend directory

--- filebank-backend/models/user.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true },
-  displayName: String,
-  email: String
-}, { timestamps: true });
-
-export default mongoose.model('User', userSchema);
