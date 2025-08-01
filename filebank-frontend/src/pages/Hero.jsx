d22566f1a72a60085ba8d473c8fd4e46121606da
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-01 07:57:30 +0200

    Update Hero.jsx

--- filebank-frontend/src/pages/Hero.jsx
+++ filebank-frontend/src/pages/Hero.jsx
@@ -371,7 +371,7 @@ export default function Hero() {
       }}>
   
 <RotatingText
-  texts={['Famacloud', 'Store', 'Files', 'image ', 'and', 'Code' ]}
+  texts={['Famacloud', 'Store', 'Files', 'images ', 'and', 'Code' ]}
   mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
   staggerFrom={"last"}
   initial={{ y: "100%" }}
