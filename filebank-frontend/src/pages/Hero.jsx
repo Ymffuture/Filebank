a8ed4579b467c002560424f0feb118a2f6b48bf7
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-01 07:33:04 +0200

    Update Hero.jsx

--- filebank-frontend/src/pages/Hero.jsx
+++ filebank-frontend/src/pages/Hero.jsx
@@ -14,6 +14,7 @@ import HeroAnimation from '../assets/bg.json';
 import Lockup from '../assets/Auth.json';
 import LockupIn from '../assets/Reg.json';
 import {Helmet} from 'react-helmet' 
+import RotatingText from './RotatingText' 
 const { Title, Paragraph, Text } = Typography;
 
 export default function Hero() {
@@ -368,7 +369,19 @@ export default function Hero() {
         color: '#777',
         fontSize: '0.85rem'
       }}>
-        Famacloud App ID:662672583632 
+  
+<RotatingText
+  texts={['React', 'Bits', 'Is', 'Cool!']}
+  mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+  staggerFrom={"last"}
+  initial={{ y: "100%" }}
+  animate={{ y: 0 }}
+  exit={{ y: "-120%" }}
+  staggerDuration={0.025}
+  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+  transition={{ type: "spring", damping: 30, stiffness: 400 }}
+  rotationInterval={2000}
+/> 
       </div>
 
     </motion.div>
