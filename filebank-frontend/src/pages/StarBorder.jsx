c3fe3569d95a20a053791a53e8418f4cc8f29b34
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-02 10:19:47 +0200

    Update StarBorder.jsx

--- filebank-frontend/src/pages/StarBorder.jsx
+++ filebank-frontend/src/pages/StarBorder.jsx
@@ -8,33 +8,36 @@ const StarBorder = ({
   ...rest
 }) => {
   return (
-    <Component 
-      className={`relative inline-block overflow-hidden rounded-[20px] ${className}`} 
-      style={{
-        padding: `${thickness}px 0`,
-        ...rest.style
-      }}
-      {...rest}
-    >
+    <div className="relative inline-block w-fit overflow-hidden rounded-[20px]">
+      <Component 
+        className={`relative z-10 inline-block rounded-[20px] ${className}`} 
+        style={{
+          padding: `${thickness}px 0`,
+          ...rest.style
+        }}
+        {...rest}
+      >
+        <div className="relative z-10 bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
+          {children}
+        </div>
+      </Component>
+
+      {/* Glow Effects */}
       <div
-        className="absolute w-[100%] h-[50%] opacity-70 bottom-[-11px] right-[-250%] rounded-full animate-star-movement-bottom z-0 overflow-x-hidden"
+        className="pointer-events-none absolute w-[150%] h-[60%] bottom-0 right-[-25%] opacity-60 rounded-full animate-star-movement-bottom z-0"
         style={{
           background: `radial-gradient(circle, ${color}, transparent 10%)`,
           animationDuration: speed,
         }}
       ></div>
       <div
-        className="absolute w-[100%] h-[50%] opacity-70 top-[-10px] left-[-250%] rounded-full animate-star-movement-top z-0"
+        className="pointer-events-none absolute w-[150%] h-[60%] top-0 left-[-25%] opacity-60 rounded-full animate-star-movement-top z-0"
         style={{
           background: `radial-gradient(circle, ${color}, transparent 10%)`,
           animationDuration: speed,
         }}
       ></div>
-      <div className="relative z-1 bg-gradient-to-b from-black to-gray-900 border border-gray-800 text-white text-center text-[16px] py-[16px] px-[26px] rounded-[20px]">
-        {children}
-      </div>
-    </Component>
+    </div>
   );
 };
 
-export default StarBorder;
