98e95dc02b07c01d3f56d58cb2302dfdeb3b8fb1
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-06 14:55:32 +0200

    Create BmcAlertWidget.jsx

---
+++ filebank-frontend/src/components/BmcAlertWidget.jsx
@@ -0,0 +1,20 @@
+// components/BmcAlertWidget.jsx
+import React from 'react';
+
+const BmcAlertWidget = () => {
+  return (
+    <div className="w-full flex justify-center mt-6">
+      <iframe
+        src="https://studio.buymeacoffee.com/stream-alert/page/ymffuture?user_key=f03a9e18-a43d-430f-bf3b-95f71c3bd1f7"
+        title="BuyMeACoffee Stream Alert"
+        width="400"
+        height="100"
+        frameBorder="0"
+        allow="autoplay"
+        className="rounded-lg shadow-md"
+      ></iframe>
+    </div>
+  );
+};
+
+export default BmcAlertWidget;
