72e1fd793c01c5f0b95882cc74fe84c1610ad582
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-26 08:45:16 +0200

    Update ShinyText.jsx

--- filebank-frontend/src/components/ShinyText.jsx
+++ filebank-frontend/src/components/ShinyText.jsx
@@ -5,8 +5,8 @@ const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
     <div
       className={`text-[#b5b5b5a4] bg-clip-text inline-block ${disabled ? '' : 'animate-shine'} ${className}`}
       style={{
-        backgroundImage: 'linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
-        backgroundSize: '200% 100%',
+        backgroundImage: 'linear-gradient(30deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
+        backgroundSize: '100% 200%',
         WebkitBackgroundClip: 'text',
         animationDuration: animationDuration,
       }}
