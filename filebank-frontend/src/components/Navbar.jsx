fecaa72d80e449f9c74a190c09703d0eb9936cc8
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-26 07:32:59 +0200

    Update Navbar.jsx

--- filebank-frontend/src/components/Navbar.jsx
+++ filebank-frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ import {
 
 
 // end
-import SL from './SearchLink';
+import search from './SearchLink';
 import ShinyText from './ShinyText';
 import { FaUserCircle } from "react-icons/fa";
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
@@ -262,7 +262,7 @@ export default function Navbar() {
   {/* Avatar and user dropdown on the right */}
   <div className="absolute right-5 top-3 flex items-center space-x-4 gap-3">
   {/* Notification Icon with Badge */}
-    <SL/>
+    <search />
   <Badge
   count={notifications > 0 ? (
     <span
