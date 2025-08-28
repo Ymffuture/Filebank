e0f41ed639d9e917ef4239d8add44f4b7d70bade
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-08-28 17:53:03 +0200

    Update ShinyText.jsx

--- filebank-frontend/src/components/ShinyText.jsx
+++ filebank-frontend/src/components/ShinyText.jsx
@@ -1,14 +1,20 @@
-const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
+const ShinyText = ({ text, disabled = false, speed = 5, className = "" }) => {
   const animationDuration = `${speed}s`;
 
   return (
     <div
-      className={`text-[#b5b5b5a4] bg-clip-text inline-block ${disabled ? '' : 'animate-shine'} ${className}`}
+      className={`text-[#b5b5b5a4] bg-clip-text inline-block ${
+        disabled ? "" : "animate-shine"
+      } ${className}`}
       style={{
-        backgroundImage: 'linear-gradient(30deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
-        backgroundSize: '100% 200%',
-        WebkitBackgroundClip: 'text',
+        backgroundImage:
+          "linear-gradient(30deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)",
+        backgroundSize: "200% 200%",
+        WebkitBackgroundClip: "text",
         animationDuration: animationDuration,
+        animationName: disabled ? "none" : "shine",
+        animationIterationCount: disabled ? "0" : "infinite",
+        animationTimingFunction: "linear",
       }}
     >
       {text}
