a45a0982219d5f29782996bafe99be25e0243315
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-10-10 02:55:53 +0200

    Update App.jsx

--- filebank-frontend/src/App.jsx
+++ filebank-frontend/src/App.jsx
@@ -36,7 +36,7 @@ const Loader = () => (
     justifyContent: 'center',
     alignItems: 'center',
     minHeight: '100vh',
-    background: '#121212'
+    background: '#fff'
   }}>
     <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
       <defs>
